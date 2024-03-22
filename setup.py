e565769e4c349e6d5f501907e32a49593eeea19a
Nemuel Wainaina <nemuelwainaina@proton.me>
2024-03-22 12:29:33 +0300

    Update project's long_description

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='mailcheck',
     description='Python wrapper for the MailCheck.ai API',
-    long_description='Python wrapper for the MailCheck.ai API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     version='0.1.0',
     packages=find_packages(),
     author='Nemuel Wainaina'
